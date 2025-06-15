71d57fa80e27dbe18ec8727b909bc92c26e85264
far0010 <far0010@alu.ubu.es>
2025-06-15 21:46:43 +0200

    Update test.js
    Update test.js

--- project-docs/memoria/test/test.js
+++ project-docs/memoria/test/test.js
@@ -56,6 +56,7 @@ test('Login erróneo', async t => {
     await t.expect(errorMessage.innerText).contains('Invalid Login Credentials');
 });
 
+
 test('Login y desplegar Administración', async t => {
     // Despliega el menú administración
     const adminMenu = Selector('#admin-menu');
@@ -86,4 +87,127 @@ test('Login y desplegar Administración', async t => {
         .click(toggleIcon)  // Hace clic en el ícono de despliegue
         .expect(subMenu.visible).ok({ timeout: 5000 }); // Verifica que el submenú esté visible
 
+});
+
+test('Login y desplegar Responsable', async t => {
+    // Despliega el menú administración
+    const adminMenu = Selector('#admin-menu');
+    const submenu = Selector('.submenu');
+    // Selecciona los campos usando el atributo 'name'
+    const usernameField = Selector('input').withAttribute('name', 'P101_USERNAME');
+    const passwordField = Selector('input').withAttribute('name', 'P101_PASSWORD');
+    const loginButton = Selector('.region-buttons').find('button').withText('LOGIN');
+    const admiMenu = Selector('.a-TreeView-label').withText('Administración');
+    const toggleIcon = Selector('.a-TreeView-toggle').nth(0); // Primer ícono de expansión
+    const subMenu = Selector('#t_TreeNav_1_subtree');
+    const RespMenu = Selector('.a-TreeView-label').withText('Responsable');
+    const responsablesHeader = Selector('h1.t-Breadcrumb-label').withText('Responsables');
+
+    await t.navigateTo('http://192.168.2.61:8080/ords/f?p=100:1:34126906504519:::::');
+
+// Espera a que los campos estén disponibles
+    await t.expect(usernameField.exists).ok({ timeout: 5000 });
+    await t.expect(passwordField.exists).ok({ timeout: 5000 });
+
+    // Interacción con los elementos
+    await t
+        .typeText(usernameField, 'user01')
+        .typeText(passwordField, 'user01')
+       
+    await clickButton();
+
+    // Hacer clic en el menú y verificar si se ha expandido
+    await t
+        .click(toggleIcon)  // Hace clic en el ícono de despliegue
+        .expect(subMenu.visible).ok({ timeout: 5000 }); // Verifica que el submenú esté visible
+
+    // click en responsable
+    await t.expect(RespMenu.exists).ok({ timeout: 5000 });
+
+    // Hace clic en el enlace "Responsables"
+     await t.click(RespMenu);
+
+     await t.expect(responsablesHeader.exists).ok({ timeout: 5000 });
+});
+
+test('Login y desplegar Proyectos', async t => {
+    // Despliega el menú administración
+    const adminMenu = Selector('#admin-menu');
+    const submenu = Selector('.submenu');
+    // Selecciona los campos usando el atributo 'name'
+    const usernameField = Selector('input').withAttribute('name', 'P101_USERNAME');
+    const passwordField = Selector('input').withAttribute('name', 'P101_PASSWORD');
+    const loginButton = Selector('.region-buttons').find('button').withText('LOGIN');
+    const admiMenu = Selector('.a-TreeView-label').withText('Administración');
+    const toggleIcon = Selector('.a-TreeView-toggle').nth(0); // Primer ícono de expansión
+    const subMenu = Selector('#t_TreeNav_1_subtree');
+    const ProyMenu = Selector('.a-TreeView-label').withText('Proyectos');
+    const ProyectosHeader = Selector('h1.t-Breadcrumb-label').withText('Proyectos');
+
+    await t.navigateTo('http://192.168.2.61:8080/ords/f?p=100:1:34126906504519:::::');
+
+// Espera a que los campos estén disponibles
+    await t.expect(usernameField.exists).ok({ timeout: 5000 });
+    await t.expect(passwordField.exists).ok({ timeout: 5000 });
+
+    // Interacción con los elementos
+    await t
+        .typeText(usernameField, 'user01')
+        .typeText(passwordField, 'user01')
+       
+    await clickButton();
+
+    // Hacer clic en el menú y verificar si se ha expandido
+    await t
+        .click(toggleIcon)  // Hace clic en el ícono de despliegue
+        .expect(subMenu.visible).ok({ timeout: 5000 }); // Verifica que el submenú esté visible
+
+    // click en responsable
+    await t.expect(ProyMenu.exists).ok({ timeout: 5000 });
+
+    // Hace clic en el enlace "Responsables"
+     await t.click(ProyMenu);
+
+     await t.expect(ProyectosHeader.exists).ok({ timeout: 5000 });
+});
+
+test('Login y desplegar Solicitante', async t => {
+    // Despliega el menú administración
+    const adminMenu = Selector('#admin-menu');
+    const submenu = Selector('.submenu');
+    // Selecciona los campos usando el atributo 'name'
+    const usernameField = Selector('input').withAttribute('name', 'P101_USERNAME');
+    const passwordField = Selector('input').withAttribute('name', 'P101_PASSWORD');
+    const loginButton = Selector('.region-buttons').find('button').withText('LOGIN');
+    const admiMenu = Selector('.a-TreeView-label').withText('Administración');
+    const toggleIcon = Selector('.a-TreeView-toggle').nth(0); // Primer ícono de expansión
+    const subMenu = Selector('#t_TreeNav_1_subtree');
+    const SoliMenu = Selector('.a-TreeView-label').withText('Solicitante');
+    const SolicitanteHeader = Selector('h1.t-Breadcrumb-label').withText('Solicitante');
+
+    await t.navigateTo('http://192.168.2.61:8080/ords/f?p=100:1:34126906504519:::::');
+
+// Espera a que los campos estén disponibles
+    await t.expect(usernameField.exists).ok({ timeout: 5000 });
+    await t.expect(passwordField.exists).ok({ timeout: 5000 });
+
+    // Interacción con los elementos
+    await t
+        .typeText(usernameField, 'user01')
+        .typeText(passwordField, 'user01')
+       
+    await clickButton();
+
+    // Hacer clic en el menú y verificar si se ha expandido
+    await t
+        .click(toggleIcon)  // Hace clic en el ícono de despliegue
+        .expect(subMenu.visible).ok({ timeout: 5000 }); // Verifica que el submenú esté visible
+
+    // click en responsable
+    await t.expect(SoliMenu.exists).ok({ timeout: 5000 });
+
+    // Hace clic en el enlace "Responsables"
+     await t.click(SoliMenu);
+
+     await t.expect(SolicitanteHeader.exists).ok({ timeout: 5000 });
 });
