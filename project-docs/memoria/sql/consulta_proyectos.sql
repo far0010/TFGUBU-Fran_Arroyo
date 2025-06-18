5e0ddc0ae508953039883863ded0d6540e77ea17
far0010 <93671310+far0010@users.noreply.github.com>
2025-06-18 19:47:34 +0200

    Delete project-docs directory

--- project-docs/memoria/sql/consulta_proyectos.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    p.ORGANICA, 
-    p.TITULO, 
-    r.NOMBRE || ' ' || r.ape1 || ' ' || r.ape2 AS REPONSABLE,
-    p.fecha_ini as INICIO, 
-    p.fecha_fin AS FIN  
-FROM proyecto p
-JOIN responsable r ON p.responsable = r.dni;
