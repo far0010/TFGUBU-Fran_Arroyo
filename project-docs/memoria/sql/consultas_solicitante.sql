2af3ea29a398264f8ed850f564b312403c0c0fd6
far0010 <far0010@alu.ubu.es>
2025-06-29 12:19:54 +0200

    Update consultas_solicitante.sql
    Actualización de las consultas del solicitante, para comprobar si está abierta la convocatoria

--- project-docs/memoria/sql/consultas_solicitante.sql
+++ project-docs/memoria/sql/consultas_solicitante.sql
@@ -1,24 +1,45 @@
+-- comprobar si convocatoria abierta
+SELECT ABIERTO
+FROM CONVOCATORIA 
+WHERE REFERENCIA = :P7_REF_CON
+
+-- muestro mensaje en js
+
+apex.message.clearErrors();
+
+const abierto = $v('P7_CON_OPEN');
+
+if (abierto == '0') {
+    const mensaje = 'Esta convocatoria está cerrada: ';
+    apex.message.alert("ℹ️ AVISO: "+mensaje);
+    $s('P7_REF_CON', ''); // Limpia la convocatoria para que elijan otra
+} 
+
+-- comprobar titulación requerida
+
 SELECT TIT_REQUERIDA
 FROM CONVOCATORIA 
 WHERE REFERENCIA = :P7_REF_CON
 
-----------------
+-- muestro mensaje en js
 
 apex.message.clearErrors();
+if ($v('P7_CON_OPEN') == '1'){
+    // compruebo que la convocatoira está abierta
+    const valReq = $v('P7_TIT-REQ');
+    const valSol = $v('P7_TIT_SOL');
 
-const valReq = $v('P7_TIT-REQ');
-const valSol = $v('P7_TIT_SOL');
+    if (valReq !== '' && valSol !== '') {
+        const tit_requerido = parseInt(valReq, 10);
+        const tit_usuario = parseInt(valSol, 10);
 
-if (valReq !== '' && valSol !== '') {
-  const tit_requerido = parseInt(valReq, 10);
-  const tit_usuario = parseInt(valSol, 10);
-
-  if (tit_usuario < tit_requerido) {
-   // Buscar el texto asociado al valor requerido
-    const select = document.getElementById('P7_TIT_SOL');
-    const textoRequerido = Array.from(select.options).find(opt => opt.value === valReq)?.text || 'nivel superior';
-    const mensaje = 'Nivel académico insuficiente. Se requiere al menos: ' + textoRequerido;
-    apex.message.alert("ℹ️ AVISO: "+mensaje);
-    $s('P7_REF_CON', ''); // Limpia la convocatoria para que elijan otra
-  }
-} 
+        if (tit_usuario < tit_requerido) {
+        // Buscar el texto asociado al valor requerido
+         const select = document.getElementById('P7_TIT_SOL');
+         const textoRequerido = Array.from(select.options).find(opt => opt.value === valReq)?.text || 'nivel superior';
+         const mensaje = 'Nivel académico insuficiente. Se requiere al menos: ' + textoRequerido;
+         apex.message.alert("ℹ️ AVISO: "+mensaje);
+         $s('P7_REF_CON', ''); // Limpia la convocatoria para que elijan otra
+        }
+    } 
+}
