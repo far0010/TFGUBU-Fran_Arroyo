d1c99a0fb0aa0fcaf952a2099e32f7171a7bd86b
far0010 <far0010@alu.ubu.es>
2025-07-16 21:48:45 +0200

    Create db-utils.js
    Necesario separar del fichero de constantes, que al finalizar con module.exports anulaba todas las exportaciones anteriores de constanst.js

---
+++ project-docs/memoria/test/db-utils.js
@@ -0,0 +1,30 @@
+import oracledb from 'oracledb';
+
+const oracledb = require('oracledb');
+
+const DB_CONFIG = {
+  user: 'TFGUBU',
+  password: '1234',
+  connectString: '192.168.2.61:1521/FREEPDB1'
+};
+
+async function getNominaCount(nif) {
+  let connection;
+  try {
+    connection = await oracledb.getConnection(DB_CONFIG);
+    const result = await connection.execute(
+      `SELECT COUNT(*) FROM NOMINA WHERE DNI_NOM = :nif`,
+      [nif]
+    );
+    return result.rows[0][0];
+  } catch (err) {
+    console.error('Error al consultar la base de datos:', err);
+    throw err;
+  } finally {
+    if (connection) {
+      await connection.close();
+    }
+  }
+}
+
+module.exports = { DB_CONFIG, getNominaCount };
